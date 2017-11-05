42303e706b1a19c906ac033a2499d58fd5b2ad5f
Bigi Lui <newbigi@gmail.com>
2017-11-04 19:03:00 -0700

    fixing first of many tab bugs: at least works right with one tab (disconnect on close)

--- background.js
+++ background.js
@@ -1,7 +1,23 @@
 var audioCtx = new (window.AudioContext)();
 var source = null, splitter = null, gainLeft = null, gainRight = null, dir = null;
+var tabId = null;
 
 chrome.extension.onConnect.addListener(function(port) {
+  // first, make sure disconnect handler sets all our vars back to null
+  chrome.tabCapture.onStatusChanged.addListener(function(info) {
+    if (info.status === 'active') {
+      tabId = info.tabId;
+    }
+    if (info.status === 'stopped' && tabId === info.tabId) {
+      tabId = source = splitter = gainLeft = gainRight = dir = null;
+    }
+  });
+  chrome.tabs.onRemoved.addListener(function(tabIdRemoved, removed) {
+    if (tabIdRemoved === tabId) {
+      tabId = source = splitter = gainLeft = gainRight = dir = null;
+    }
+  })
+  // now listens for any messages from popup
   port.onMessage.addListener(function(msg) {
     if (msg.action === 'start') {
       if (source === null) {
